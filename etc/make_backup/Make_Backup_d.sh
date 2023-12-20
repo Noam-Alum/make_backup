f94f00f60435ea335b08c460f6608ed1ef0af8b4
Noam Alum <132448828+Noam-Alum@users.noreply.github.com>
2023-12-20 19:07:31 +0200

    Update Make_Backup_d.sh

--- etc/make_backup/Make_Backup_d.sh
+++ etc/make_backup/Make_Backup_d.sh
@@ -4,47 +4,34 @@
 # Make Backup Demon
 
 ### FUNCTIONS
-## Read config
-function read_config {
-    ## GET LIST ALL NEEDED FILES
-    export Files="$(cat /etc/make_backup/Make_Backup.conf | sed -n '/> start items to backup <$/,/> end items to backup <$/{//!p}')"
-    if [ -z "$Files" ]; then
-        exit 1
-    fi
-
-    # GET CONF VARS
-    conf_vars="bd_count backup_in_c_month backup_in_month month_in_c_year month_in_year rm_old_backups"
-    for c_var in $conf_vars
-    do
-        export $c_var="$(cat /etc/make_backup/Make_Backup.conf | awk -F "$c_var=\[" {'print $2'} | awk -F '\];' {'print $1'} | tr -d '[:space:]')"
-        if [ -z "$c_var" ]; then
-            exit 1
-        fi
-    done
-
-    ## GET BACKUP DIR
-    export BACKUP_dir="$(cat /etc/make_backup/Make_Backup.conf | awk -F 'parent_directory=\[' '{print $2}' | awk -F '\];' {'print $1'} | tr -d '[:space:]')"
-    if [ -z "$BACKUP_dir" ]; then
-        exit 1
-    elif [ ! -e "$BACKUP_dir" ]; then
-        # SET BACKUP_dir to backup BACKUP_dir to avoid crashing
-        local fallback_directory="$(cat /etc/make_backup/Make_Backup.conf | awk -F "fallback_directory=\[" {'print $2'} | awk -F '\];' {'print $1'} | tr -d '[:space:]')"
-        export BACKUP_dir=$fallback_directory
-
-        # set old backup removal to no
-        export rm_old_backups="no"
-    fi
-}
-
 ## LOG
 function AddLog {
     # VARS
     local Tag="$1"
     shift
     local Message="$*"
+    
+    # get binaries
+    # search for binary
+    if [ ! -z "$(which logger)" ]; then
+        # set command to full path to binary
+        export logger="$(which logger)"
+
+    # try running the command if so use it as is
+    elif [ ! -z "$(logger --help 2>/dev/null)" ]; then
+        export logger="logger"
+        
+    # binary dependency missing exiting
+    else
+        # LOG
+        AddLog "ERROR" "binary dependency missing \"logger\" exiting."
+            
+        # EXIT
+        exit 1
+    fi
 
     # LOG
-    logger -p local0.info -t "[$Tag]" "$Message"
+    $logger -p local0.info -t "[$Tag]" "$Message"
 }
 
 ## handle errors
@@ -55,15 +42,95 @@ function HandleError {
 }
 trap 'HandleError' ERR
 
+## Get binary
+function get_binary {
+    trap 'HandleError' ERR
+
+    # list all binaries
+    mbn_commands=("cat" "date" "sed" "awk" "tr" "sort" "tail" "rm" "ls" "mkdir" "head" "rsync" "echo")
+
+    # get binaries
+    for binary in ${mbn_commands[@]}
+    do
+        # search for binary
+        if [ ! -z "$(which $binary)" ]; then
+            # set command to full path to binary
+            export $binary="$(which $binary)"
+
+        # try running the command if so use it as is
+        elif [ ! -z "$($binary --help 2>/dev/null)" ];then
+            export $binary="$binary"
+        
+        # binary dependency missing exiting
+        else
+            # LOG--help
+            AddLog "ERROR" "binary dependency missing \"$binary\" exiting."
+            
+            # EXIT
+            exit 1
+        fi
+    done
+}
+
+## Read config
+function read_config {
+    
+    #### GET BINARY
+    get_binary
+
+    trap 'HandleError' ERR
+
+    ## GET LIST ALL NEEDED FILES
+    export Files="$($cat /etc/make_backup/Make_Backup.conf | $sed -n '/> start items to backup <$/,/> end items to backup <$/{//!p}')"
+    if [ -z "$Files" ]; then
+        AddLog "ERROR" error while reading items to backup, exiting.
+        exit 1
+    fi
+
+    ## GET COUNT LOCATION
+    export count_location="$($cat /etc/make_backup/Make_Backup.conf | $awk -F "count_location=\[" {'print $2'} | $awk -F '\];' {'print $1'} | $tr -d '[:space:]')"
+    if [ -z "$count_location" ] || [ ! -f "$count_location" ]; then
+        AddLog "ERROR" count file "$count_location" could not be used, exiting.
+        exit 1
+    fi
+
+    # GET CONF VARS
+    conf_vars="bd_count backup_in_c_month backup_in_month month_in_c_year month_in_year rm_old_backups"
+    for c_var in $conf_vars
+    do
+        export $c_var="$($cat /etc/make_backup/Make_Backup.conf | $awk -F "$c_var=\[" {'print $2'} | $awk -F '\];' {'print $1'} | $tr -d '[:space:]')"
+        if [ -z "$c_var" ]; then
+            AddLog "ERROR" error while allocating \"$c_var\", exiting.
+            exit 1
+        fi
+    done
+
+    ## GET BACKUP DIR
+    export BACKUP_dir="$($cat /etc/make_backup/Make_Backup.conf | $awk -F 'parent_directory=\[' '{print $2}' | $awk -F '\];' {'print $1'} | $tr -d '[:space:]')"
+    if [ -z "$BACKUP_dir" ] || [ ! -e "$BACKUP_dir" ]; then
+        # SET BACKUP_dir to backup BACKUP_dir to avoid crashing
+        local fallback_directory="$($cat /etc/make_backup/Make_Backup.conf | $awk -F "fallback_directory=\[" {'print $2'} | $awk -F '\];' {'print $1'} | $tr -d '[:space:]')"
+        export BACKUP_dir=$fallback_directory
+
+        # set old backup removal to no
+        export rm_old_backups="no"
+    fi
+}
+
 ## keep backups
 function keep_backups {
+    trap 'HandleError' ERR
+
+    #### GET BINARY
+    get_binary
+
     #### READ CONFIG ####
     read_config
 
     # CHECK IF THERE ARE BACKUPS IN THE Laptop_backups DIR
-    if [ ! -z "$(ls $BACKUP_dir/ 2> /dev/null)" ] && [ "$rm_old_backups" == "yes" ]; then
+    if [ ! -z "$($ls $BACKUP_dir/ 2> /dev/null)" ] && [ "$rm_old_backups" == "yes" ]; then
         # GET BIGGEST YEAR
-        biggest_year="$BACKUP_dir/$(ls $BACKUP_dir 2> /dev/null | sort -n | tail -1)"
+        biggest_year="$BACKUP_dir/$($ls $BACKUP_dir 2> /dev/null | $sort -n | $tail -1)"
 
         # GEt ALL YEARS DIRECTORIES
         for year_dir in $BACKUP_dir/*
@@ -75,7 +142,7 @@ function keep_backups {
                 month_count=0
 
                 # GET BIGGEST MONTH
-                biggest_month="$year_dir/$(ls $year_dir 2> /dev/null| sort -n | tail -1)"
+                biggest_month="$year_dir/$($ls $year_dir 2> /dev/null| $sort -n | $tail -1)"
 
                 # GEt ALL MONTH DIRECTORIES
                 for month_dir in $year_dir/*
@@ -84,8 +151,8 @@ function keep_backups {
 
                     # CHECK IF COUNT IS OVER month_in_year
                     if [ $month_count -gt $month_in_c_year ]; then
-                        rm -rf $year_dir/$(ls -t --time=atime -1 $year_dir 2> /dev/null | tail -1)
-                        AddLog "CLEARED SPACE" removed old month directory \"$year_dir/$(ls -t --time=atime -1 $year_dir 2> /dev/null | tail -1)\"
+                        $rm -rf $year_dir/$($ls -t --time=atime -1 $year_dir 2> /dev/null | $tail -1)
+                        AddLog "CLEARED SPACE" removed old month directory \"$year_dir/$($ls -t --time=atime -1 $year_dir 2> /dev/null | $tail -1)\"
                     fi
                     
                     # CHECK IF MONTH_DIR IS THE BIGGEST MONTH
@@ -102,8 +169,8 @@ function keep_backups {
                             # CHECK IF COUNT IS OVER backup_in_c_month
                             if [ $backup_count -gt $backup_in_c_month ]; then
                                 # REAMOVE OLDEST BACKUP IN MONTH DIR
-                                rm -rf $month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)
-                                AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)\"
+                                $rm -rf $month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)
+                                AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)\"
                             fi
                         done
                     else
@@ -119,8 +186,8 @@ function keep_backups {
                             # CHECK IF COUNT IS OVER backup_in_c_month
                             if [ $backup_count -gt $backup_in_month ]; then
                                 # REAMOVE OLDEST BACKUP IN MONTH DIR
-                                rm -rf $month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)
-                                AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)\"
+                                $rm -rf $month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)
+                                AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)\"
                             fi
                         done
                     fi
@@ -135,8 +202,8 @@ function keep_backups {
                     month_count=$(( $month_count + 1 ))
                     # CHECK IF COUNT IS OVER month_in_year
                     if [ $month_count -gt $month_in_year ]; then
-                        rm -rf $year_dir/$(ls -t --time=atime -1 $year_dir 2> /dev/null | tail -1)
-                        AddLog "CLEARED SPACE" removed old month directory \"$year_dir/$(ls -t --time=atime -1 $year_dir 2> /dev/null | tail -1)\"
+                        $rm -rf $year_dir/$($ls -t --time=atime -1 $year_dir 2> /dev/null | $tail -1)
+                        AddLog "CLEARED SPACE" removed old month directory \"$year_dir/$($ls -t --time=atime -1 $year_dir 2> /dev/null | $tail -1)\"
                     fi
 
 
@@ -152,8 +219,8 @@ function keep_backups {
                         # CHECK IF COUNT IS OVER backup_in_c_month
                         if [ $backup_count -gt $backup_in_month ]; then
                             # REAMOVE OLDEST BACKUP IN MONTH DIR
-                            rm -rf $month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)
-                            AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$(ls -t --time=atime -1 $month_dir 2> /dev/null | tail -1)\"
+                            $rm -rf $month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)
+                            AddLog "CLEARED SPACE" removed old backup directory \"$month_dir/$($ls -t --time=atime -1 $month_dir 2> /dev/null | $tail -1)\"
                         fi
                     done  
                 done
@@ -163,30 +230,61 @@ function keep_backups {
 }
 
 while true; do
-
     #### READ CONFIG ####
     read_config
+
+    #### GET BINARY
+    get_binary
+
+    #### CATCH UDEV ####
+    if [ "$1" == "UDEV" ]; then
+        if [ ! -s "$count_location" ] || [ $(cat "$count_location") -gt $bd_count ]; then
+            # START COUNT
+            echo '0' > $count_location
+
+            # LOG
+            AddLog "STARTING-COUNT" "set count to \"0\" count file \"$count_location\" was empty or above \"$bd_count\"."
+
+            # EXIT
+            exit 0
+        elif [[ "$(cat "$count_location")" =~ ^[0-9]+$ ]]; then
+            # LOG
+            AddLog "CHANGING-COUNT" "changing count to \"$(( $(cat $count_location) + 1 ))\"."
+
+            # ADD COUNT
+            echo $(( $(cat $count_location) + 1 )) > $count_location
+
+            # EXIT
+            exit 0
+        else
+            # LOG
+            AddLog "ERROR" "count file $count_location is not numeric"
+
+            # EXIT
+            exit 1
+        fi
+    fi
 
     #### KEEP BACKUPS NEEDED ####
     keep_backups
 
     #### CHECK IF COUNT IS BIGGER THAN 10 ####
-    if [ $(cat /var/Make_Backup/count.txt) -eq $bd_count ]; then
+    if [ $($cat $count_location) -eq $bd_count ]; then
             sleep 10
             ### START SCRIPT
 
             ## IF NOT EXIST CREATE YEAR AND MONTH DIRS
             # YEAR
-            year_dir="$(date '+%Y')"
+            year_dir="$($date '+%Y')"
 
             # MONTH
-            month_dir="$(date '+%m')"
+            month_dir="$($date '+%m')"
 
-            mkdir -p $BACKUP_dir/$year_dir/$month_dir
+            $mkdir -p $BACKUP_dir/$year_dir/$month_dir
 
             ## CREATE BACKUP DIRECTORY
-            BACKUP_dir="$BACKUP_dir/$year_dir/$month_dir/$(openssl rand -base64 6 | tr -d '/')_$(date '+%d_%H_%M')/"
-            mkdir "$BACKUP_dir"
+            BACKUP_dir="$BACKUP_dir/$year_dir/$month_dir/$($tr -dc 'a-zA-Z0-9' < /dev/random | $head -c 6)_$($date '+%d_%H_%M')/"
+            $mkdir "$BACKUP_dir"
             
             ## BACKUP FILES
             # LOG
@@ -197,13 +295,13 @@ while true; do
             do
                 # RSYNC
                 AddLog "BACKUPING ITEM" "$File"
-                rsync -av --relative "$File" "$BACKUP_dir" &> /dev/null
+                $rsync -av --relative "$File" "$BACKUP_dir" &> /dev/null
             done
 
             # LOG
             AddLog "FINISHED BACKUP" backup at $BACKUP_dir
             
-            echo "0" > /var/Make_Backup/count.txt
+            $echo "0" > $count_location
             # LOG
             AddLog "RESTORING COUNT" setting activation count to \"0\".
 
